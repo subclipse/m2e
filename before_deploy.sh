aa242ff70f21c652bd21d213d500a9209d4f6c4f
Mark Phippard <markphip@gmail.com>
2016-09-10 08:47:48 -0400

    Fix paths in before_deploy script

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-cat ./releng/tag.json | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./tag.json
-cat ./releng/release-compositeArtifacts.xml | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./release-compositeArtifacts.xml
-cat ./releng/release-compositeContent.xml | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./release-compositeContent.xml
-cat ./releng/latest-compositeArtifacts.xml | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./latest-compositeArtifacts.xml
-cat ./releng/latest-compositeContent.xml | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./latest-compositeContent.xml
+cat ./releng/release/tag.json | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./tag.json
+cat ./releng/release/release-compositeArtifacts.xml | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./release-compositeArtifacts.xml
+cat ./releng/release/release-compositeContent.xml | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./release-compositeContent.xml
+cat ./releng/release/latest-compositeArtifacts.xml | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./latest-compositeArtifacts.xml
+cat ./releng/release/latest-compositeContent.xml | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./latest-compositeContent.xml
