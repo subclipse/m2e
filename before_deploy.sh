5032da13cc252058c6334cbaf72862732256a0b5
Mark Phippard <markphip@gmail.com>
2016-09-10 09:01:37 -0400

    Update before_deploy to setup the release/latest

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 cat ./releng/release/tag.json | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./tag.json
+cat ./releng/release/release.json | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./release.json
+cat ./releng/release/latest.json | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./latest.json
 cat ./releng/release/release-compositeArtifacts.xml | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./release-compositeArtifacts.xml
 cat ./releng/release/release-compositeContent.xml | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./release-compositeContent.xml
 cat ./releng/release/latest-compositeArtifacts.xml | sed s/\$\{TRAVIS_TAG\}/${TRAVIS_TAG}/g > ./latest-compositeArtifacts.xml
